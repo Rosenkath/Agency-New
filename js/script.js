14b00d6da3fbd8cf53663d1b9552344ad26f14b7
Rodrigo <rodrigocbs913@gmail.com>
2021-07-31 00:49:19 -0300

    Primeiro commit deste site

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('#nav-toggle').click(function(e){
+       e.preventDefault();
+        $(this).toggleClass('active');
+        $('.header-collapse').toggleClass('active');
+    })
+    //Caroulsel Principal
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        lazyLoad:true,
+        loop:true,
+        margin:10,
+        nav: true,
+        navText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>']
+    });
+});

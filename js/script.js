c929bd8b3e5634ed2373211e9520022cd063cc19
Rodrigo <rodrigocbs913@gmail.com>
2021-08-02 20:15:11 -0300

    Adicionando blocos de Serviços e Testemunhas

--- js/script.js
+++ js/script.js
@@ -19,4 +19,27 @@ $(document).ready(function(){
         autoplaySpeed: 1000,
         responsiveRefreshRate: 10
     });
+        //carousel testemunhas
+    $('#carousel-testemunhas').owlCarousel({
+        items:2,
+        loop:true,
+        margin:40,
+        nav: false,
+        navSpeed:1000,
+        navText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+        dots: true,
+        dotsSpeed: 1000,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        responsiveRefreshRate: 10,
+        responsive:{
+            960:{
+                items:2
+            },
+            1280:{
+                items:2,
+                nav: true
+            }
+        }
+    });
 });

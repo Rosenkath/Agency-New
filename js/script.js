d8923ae3b944199bf0bf4ecfd8192fdcf84bfc10
Rodrigo <rodrigocbs913@gmail.com>
2021-08-06 22:33:42 -0300

    otimizando o esquema de scroll do site

--- js/script.js
+++ js/script.js
@@ -4,6 +4,64 @@ $(document).ready(function(){
         $(this).toggleClass('active');
         $('.header-collapse').toggleClass('active');
     })
+
+    //Scroll
+
+    var nav = $('.header-nav'),
+        navHeight = nav.outerHeight(),
+        sections = $('.section');
+
+    //fixando header
+
+    $(window).on('scroll', function(){
+        var sTop = $(this).scrollTop();
+        if(sTop > navHeight){
+            $('.header').addClass('fixed')
+        }else{
+            $('.header').removeClass('fixed')
+        }
+
+        //marcando menu scroll
+        if(sTop == 0){
+            nav.find('a').removeClass('active')
+            nav.find('a[href="#home"]').addClass('active')
+        }else{
+        sections.each(function(){
+            var top = $(this).offset().top - navHeight;
+
+            if(sTop >= top){
+                nav.find('a').removeClass('active')
+                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active')
+                }
+            })     
+        }
+    });
+
+    //navegação
+
+    nav.find('a').on('click', function(e){
+        e.preventDefault();
+        $('.header-collapse').removeClass('active'); 
+        $('#nav-toggle').removeClass('active'); 
+
+
+        var target = $(this).attr('href');
+        if(target == "#home"){
+            $('html, body').animate({scrollTop: 0}, 700);
+        }else{
+            $('html, body').stop().animate({
+                scrollTop: $(target).offset().top}
+                , 700);
+        }
+    });
+
+    //back top
+
+    $('.back-top').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 700)
+    });
+
     //Caroulsel Principal
     $('#carousel-principal').owlCarousel({
         items:1,
